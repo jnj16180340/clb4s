bac15b9fc5a067f37db0c105ee7b0c5c5dd449e4
nate johnson <nate@biobright.com>
2019-03-09 01:32:19 -0500

    The controller appears to work. Button combos are too slow to work tho

--- sc2/interface.py
+++ sc2/interface.py
@@ -5,7 +5,7 @@
 
 from os import getenv
 from time import sleep
-# ser = serial.Serial(args.port, 9600)
+from serial import Serial
 from time import sleep 
 
 DEFAULT_PORT='/dev/ttyUSB0'
@@ -46,65 +46,63 @@ class SwitchController():
                              "CENTER",  # 128
                              "MAX"}     # 255
 
-    valid_hat_positions = {"TOP"
-                           "TOP_RIGHT"
-                           "RIGHT"
-                           "BOTTOM_RIGHT"
-                           "BOTTOM"
-                           "BOTTOM_LEFT"
-                           "LEFT"
-                           "TOP_LEFT"
+    valid_hat_positions = {"TOP",
+                           "TOP_RIGHT",
+                           "RIGHT",
+                           "BOTTOM_RIGHT",
+                           "BOTTOM",
+                           "BOTTOM_LEFT",
+                           "LEFT",
+                           "TOP_LEFT",
                            "CENTER"}
 
-    def block_on_connect(port='/dev/ttyUSB0'): 
-        p = Path(port)
-        elapsed = 0
+    def block_on_connect(self): 
+        p = Path(self.port)
+        elapsed = float(0)
         while not p.exists():
-            print(f'Waiting {elapsed}s for controller on {port}...')
+            print(f'Waiting {elapsed}s for controller on {self.port}...')
             sleep(0.25)
         print('...Got it!') 
         return str(p)
 
-    def __enter__():
+    def __enter__(self):
         if self.boc:
             block_on_connect()
         print(f'Opening {self.port}')
-        self.ser = serial.Serial(self.port, self.baudrate)
-        ser.clear_input_buffer()
-        ser.clear_output_buffer()
-        return ser
+        self.ser = Serial(self.port, self.baudrate)
+        return self
 
     def __exit__(self, *args):
         print(f'Closing {self.port}')
         self.ser.close()
 
-    @classmethod
+    @staticmethod
     def fcmd(msg):
         return f'{msg}\r\n'.encode('utf-8')
 
     def send(self, cmd, dur=0.1, rel=True):
-        self.ser.write(format_command(cmd))
+        self.ser.write(self.fcmd(cmd))
         sleep(dur)
         if rel:
             self.ser.write(b'RELEASE\r\n')
 
     def release(self, **kwargs):
-        send(fcmd('RELEASE'), **kwargs)
+        self.send('RELEASE', **kwargs)
 
     def button(self, b, **kwargs):
-        if b not in valid_buttons:
+        if b not in self.valid_buttons:
             raise BadButtonError(b)
-        send(fcmd(f'Button {b}'))
+        self.send(f'Button {b}', **kwargs)
 
-    def stick(s, p, **kwargs):
-        if s not in valid_sticks:
+    def stick(self, s, p, **kwargs):
+        if s not in self.valid_sticks:
             raise BadButtonException
-        if p not in valid_stick_positions:
+        if p not in self.valid_stick_positions:
             raise BadButtonError(s, p)
-        send(fcmd(f'{s} {p}'), **kwargs)
+        self.send(f'{s} {p}', **kwargs)
 
-    def hat(p, **kwargs):
-        if p not in valid_hat_positions:
+    def hat(self, p, **kwargs):
+        if p not in self.valid_hat_positions:
             raise BadButtonError(p)
-        send(fcmd(f'HAT {p}'), **kwargs)
+        self.send(f'HAT {p}', **kwargs)
 
